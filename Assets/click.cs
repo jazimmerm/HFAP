0f0246f99677eff8be3a906acc103f8e85386e27
ines said <inessaid@mail.usf.edu>
2020-08-15 17:14:24 -0400

    first commit

---
+++ Assets/click.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class click : MonoBehaviour
+{
+   // public GameObject yourButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Button btn = yourButton.GetComponent<Button>();
+        //btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        //Debug.Log("You have clicked the button!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        // Destroy the gameObject after clicking on it
+        Destroy(gameObject);
+    }
+}
