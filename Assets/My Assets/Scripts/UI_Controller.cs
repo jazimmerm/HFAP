660619c5b908c93b8eb04fa4b1e5b0cc906a78dd
stanburya <67989967+stanburya@users.noreply.github.com>
2020-08-16 04:42:12 -0400

    This is not really a change

---
+++ Assets/My Assets/Scripts/UI_Controller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Controller : MonoBehaviour
+{
+    public GameObject kenyaUI;
+    public GameObject ghanaUI;
+    public GameObject botswanaUI;
+    public GameObject kenyaButton;
+    public GameObject ghanaButton;
+    public GameObject botswanaButton;
+
+    string btnName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        kenyaUI.SetActive(false);
+        ghanaUI.SetActive(false);
+        botswanaUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        btnName = Hit.transform.name;
+        switch (btnName)
+                {
+                    case "kenyaButton":
+                    kenyaUI.SetActive(true);
+                    break;
+                    default:
+                    break;
+                }
+          
+       
+    }
+}
--- Assets/click.cs
+++ Assets/click.cs
@@ -29,3 +29,4 @@ public class click : MonoBehaviour
         Destroy(gameObject);
     }
 }
+
