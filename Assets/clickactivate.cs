ac282597ee36621dea37f60ce1a1696325e76443
ines said <inessaid@mail.usf.edu>
2020-08-16 06:48:43 -0400

    clickable ui

---
+++ Assets/clickactivate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickactivate : MonoBehaviour
+{
+    public GameObject Pedestale;
+    // Start is called before the first frame update
+
+    void OnMouseDown()
+    {
+        Pedestale.SetActive(!Pedestale.activeSelf);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
