7280546cafd292e1b9cd765d84d51b84e6d8baf4
ines said <inessaid@mail.usf.edu>
2020-08-16 07:55:28 -0400

    url working

---
+++ Assets/gotourl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gotourl : MonoBehaviour
+{
+
+    public string url;
+
+    // Start is called before the first frame update
+    void OnMouseDown()
+    {
+        Application.OpenURL( url);
+    }
+
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
